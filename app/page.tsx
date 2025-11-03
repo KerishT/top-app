24f175ba01d47473d79f8495ecaaafecfa56d3f2
KerishT <kerish.turista@gmail.com>
2025-11-03 12:08:14 +0300

    Edit main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,3 @@
-import {
-  ArticleListItem,
-  Button,
-  Card,
-  Htag,
-  Likes,
-  P,
-  Tag,
-} from "@/components";
-
 export default function Home() {
-  return (
-    <>
-      <Htag>123</Htag>
-      <Button arrow="right">Button</Button>
-      <Button appearance="ghost" arrow="down">
-        Button
-      </Button>
-      <P>Test</P>
-      <Tag size="s" color="red" href="/">
-        Test
-      </Tag>
-
-      <Card>
-        <ArticleListItem
-          image="/next.svg"
-          tag="Front-end"
-          date="1 месяц назад"
-          title="Как работать с CSS Grid"
-          likes={4}
-          description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..."
-          timeToRead="3 минуты"
-          href="/"
-        />
-      </Card>
-    </>
-  );
+  return <></>;
 }
