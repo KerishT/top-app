2898958c81e04e7301f25cddc1485fbd66341d46
KerishT <kerish.turista@gmail.com>
2025-11-03 10:17:40 +0300

    Add Tag, refactor Htag

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import { Button, Htag, P } from "@/components";
+import { Button, Htag, P, Tag } from "@/components";
 
 export default function Home() {
   return (
     <>
-      <Htag tag="h1">123</Htag>
+      <Htag>123</Htag>
       <Button arrow="right">Button</Button>
       <Button appearance="ghost" arrow="down">
         Button
       </Button>
       <P>Test</P>
+      <Tag size="s" color="red" href="/">
+        Test
+      </Tag>
     </>
   );
 }
