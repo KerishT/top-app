474edbf061e4ddd4397808aff7f078e0e48ef3b2
KerishT <kerish.turista@gmail.com>
2025-11-26 08:33:26 +0300

    Add Providers, GithubContext, useGithub

--- app/(site)/components/Layout/Footer/Footer.tsx
+++ app/(site)/components/Layout/Footer/Footer.tsx
@@ -1,18 +1,22 @@
+"use client";
 import clsx from "clsx";
 import { format } from "date-fns";
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
+import { useGithub } from "@/app/(site)/contexts";
 
 export const Footer = ({ className, ...props }: FooterProps) => {
+  const githubLink = useGithub();
+
   return (
     <footer className={clsx(className, styles.footer)} {...props}>
       <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
 
-      <a href="#" target="_blank">
+      <a href={githubLink} target="_blank">
         Пользовательское соглашение
       </a>
 
-      <a href="#" target="_blank">
+      <a href={githubLink} target="_blank">
         Политика конфиденциальности
       </a>
     </footer>
---
+++ app/(site)/contexts/index.ts
@@ -0,0 +1,2 @@
+export { Providers } from "./providers";
+export { useGithub } from "./github";
