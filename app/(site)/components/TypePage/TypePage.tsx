6232c556729ef9f6bb38d69c806d7ab4d0420df8
KerishT <kerish.turista@gmail.com>
2025-12-02 08:03:57 +0300

    Add Card, HhData

--- app/(site)/components/TypePage/TypePage.tsx
+++ app/(site)/components/TypePage/TypePage.tsx
@@ -1,5 +1,38 @@
+import { HhData, Htag, Tag } from "@/components";
+import { TopLevelCategory } from "@/interfaces/page.interface";
+import styles from "./TypePage.module.css";
 import { TypePageProps } from "./TypePage.props";
 
 export const TypePage = ({ page, products, firstCategory }: TypePageProps) => {
-  return <>{products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+
+        <span>Сортировка</span>
+      </div>
+
+      <div>
+        {products && products.map(p => <div key={p._id}>{p.title}</div>)}
+      </div>
+
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
+      </div>
+
+      {firstCategory == TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+    </div>
+  );
 };
--- components/index.ts
+++ components/index.ts
@@ -6,3 +6,4 @@ export * from "./Card/Card";
 export * from "./Likes/Likes";
 export * from "./Rating/Rating";
 export * from "./Article/ArticleList/ArticleList";
+export * from "./HhData/HhData";
